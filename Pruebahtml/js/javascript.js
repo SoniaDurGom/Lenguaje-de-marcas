17d0545bee1110207e4a7fe322ef4b362dc5dcce
SoniaDurGom <91540323+SoniaDurGom@users.noreply.github.com>
2021-10-07 22:14:47 +0200

    Ficheros de la práctica HTML Indice

---
+++ Pruebahtml/js/javascript.js
@@ -0,0 +1,5 @@
+function miniVentana()
+{
+	alert("Hola Mundo, soy una ventana emergente que no hace nada");
+ 	//window.open("ventana1","width=300,height=300,scrollbars=NO"); //Esto abriria ota ventana. Tal cual está no va a ningún lado
+}
